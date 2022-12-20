c7bebcb09d1f8f8c8f505a654169096c47cf0b23
Lilly Daniell <lilly.daniell@engineerbetter.com>
2022-12-20 13:48:56 +0000

    Solution day 2, part 2

--- 2/lib/2_solution.rb
+++ 2/lib/2_solution.rb
@@ -7,6 +7,8 @@ class RockPaperScissor
     @file = file
     @running_score = 0
     @results = format_file
+    @weapon_conversion = { "A" => "X", "B" => "Y", "C" => "Z" }
+    @weapons_selected = []
   end
 
   def format_file
@@ -21,7 +23,6 @@ class RockPaperScissor
   end
 
   def weapon_score
-    score = 0
     @results.each do |weapon|
       @running_score += 1 if weapon[1] == "X"
       @running_score += 2 if weapon[1] == "Y"
@@ -29,13 +30,47 @@ class RockPaperScissor
     end
   end
 
-  def calculate_total
+  def calculate_total_part_1
     weapon_score
     game_score
     puts running_score
   end
 
+  def choose_weapon
+    @results.each do |weapon|
+      @weapons_selected << @weapon_conversion[weapon[0]] if weapon[1] == "Y"
+      @weapons_selected << @weapon_conversion["B"] if weapon[1] == "Z" && weapon[0] == "A"
+      @weapons_selected << @weapon_conversion["C"] if weapon[1] == "Z" && weapon[0] == "B"
+      @weapons_selected << @weapon_conversion["A"] if weapon[1] == "Z" && weapon[0] == "C"
+      @weapons_selected << @weapon_conversion["A"] if weapon[1] == "X" && weapon[0] == "B"
+      @weapons_selected << @weapon_conversion["B"] if weapon[1] == "X" && weapon[0] == "C"
+      @weapons_selected << @weapon_conversion["C"] if weapon[1] == "X" && weapon[0] == "A" 
+    end
+  end
+
+  def weapon_chosen_score
+    choose_weapon
+    @weapons_selected.each do |weapon|
+      @running_score += 1 if weapon == "X"
+      @running_score += 2 if weapon == "Y"
+      @running_score += 3 if weapon == "Z"
+    end
+  end
+
+  def pre_determine_game_score
+    @results.each do |weapon|
+      @running_score += 3 if weapon[1] == "Y"
+      @running_score += 6 if weapon[1] == "Z"
+    end
+  end
+
+  def calculate_total_part_2
+    weapon_chosen_score
+    pre_determine_game_score
+    puts running_score
+  end
+
 end
 
-RockPaperScissor.new("input.txt").calculate_total
-
+RockPaperScissor.new("input.txt").calculate_total_part_1
+RockPaperScissor.new("input.txt").calculate_total_part_2
--- 2/spec/2_solution_spec.rb
+++ 2/spec/2_solution_spec.rb
@@ -3,14 +3,19 @@ require "2_solution"
 describe "Rock Paper Scissors" do
   it "formats input" do
     test = RockPaperScissor.new("test_input.txt")
-    p test
     expect(test.format_file).to eq([["A", "Y"], ["B", "X"], ["C", "Z"]])
   end
 
-  it "calculates total of one game" do
+  it "calculates total of one game (part 1 logic)" do
     test = RockPaperScissor.new("test_input.txt")
-    test.weapon_score
-    test.game_score
+    test.calculate_total_part_1
     expect(test.running_score).to eq(15)
   end
+
+  it "calculates total of one game (part 2 logic)" do
+    test = RockPaperScissor.new("test_input.txt")
+    test.calculate_total_part_2
+    expect(test.running_score).to eq(12)
+  end
+
 end
